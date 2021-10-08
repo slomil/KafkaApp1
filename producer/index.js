b26e3b1a6444d1cbddd8da043469daefa791b135
Slobodan Milutinovic <slobodan.milutinovic7@gmail.com>
2021-10-08 13:38:47 +0200

    Demonstration of producer and consumer in kafka

---
+++ producer/index.js
@@ -0,0 +1,23 @@
+console.log("producer..");
+import Kafka from 'node-rdkafka';
+import eventType from '../eventType.js';
+
+const stream=Kafka.Producer.createWriteStream({
+    'metadata.broker.list':'localhost:9092'},{},{topic:'test'});
+
+
+function queueMessage(){
+    const event= {category:'DOG', noise:'bark'};
+
+    const success=stream.write(eventType.toBuffer(event));
+    if(success){
+        console.log('message wrote successfully');
+    }else{
+        console.log('something went wrong..');
+    }
+    
+}
+
+setInterval(()=>{
+    queueMessage();
+},3000)
